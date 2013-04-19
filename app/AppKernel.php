402daf3dadb7a127b3f7c5b5c004366bbccb44d1
Art Willow Hundiak <ahundiak@zayso.org>
2013-04-19 16:34:20 -0500

    Basic layout

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,9 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            
+            new Cerad\Bundle\TournBundle\CeradTournBundle(),
+            new Cerad\Bundle\AppBundle\CeradAppBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
